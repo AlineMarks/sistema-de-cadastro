ab1de6255d4183465dc585d32fa1ae70a72d3faa
AlineMarks <113206932+AlineMarks@users.noreply.github.com>
2022-10-10 21:00:35 -0300

    adicionando variavel no app.py

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 print  'rodando o sistema de gerenciamento de jogos'
+<<<<<<< HEAD
+app = 'voce consegue '
+print app
+=======
 print 'aplicação está se conectando ao banco de dados'
+
+>>>>>>> 8a6db96026bce2c2211883a31c52fe154e2bb170
